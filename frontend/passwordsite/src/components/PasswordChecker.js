3460970e79fdd41a5f936997a5fad56e6043a6dc
Thanh-Huy1104 <etienne01.nguyen@gmail.com>
2023-01-21 15:46:33 -0500

    fix mount

--- frontend/passwordsite/src/components/PasswordChecker.js
+++ frontend/passwordsite/src/components/PasswordChecker.js
@@ -4,24 +4,26 @@ import {LinearProgress} from '@mui/material';
 var checkFor = require('zxcvbn');
 
 const PasswordChecker = () => {
-  const [result, setResult] = useState({});
+  const [result, setResult] = useState({ PasswordScore : {} });
 
   const setPassword = (event) => {
-    let PasswordScore = checkFor(event.target.value);
-    setResult(result => ({
-      PasswordScore
-    }));
+    if (event.target.value !== '') {
+      let PasswordScore = checkFor(event.target.value);
+      setResult(result => ({
+        PasswordScore
+      }));
+    }
   }
 
   return (
     <div className='input__field__wrapper'>
         <div className='input__field__container'>
-            <input onChange={(event) => setPassword(event)} type="text" id="password" defaultValue={result.item1} className="input__field" placeholder='Password'>
+            <input onChange={(event) => setPassword(event)} type="text" id="password" className="input__field" placeholder='Password'>
             </input>
         </div>
         <div>
-            <pre className='input__text' defaultValue={60}>
-              Password Score: {JSON.stringify(result['PasswordScore'], null, 1)}
+            <pre className='input__text'>
+              Password Score: {JSON.stringify(result['PasswordScore']['score'], null, 1)}
               
             </pre>
             <LinearProgress variant='determinate' value={60} />
