eac2c7d3844fcabe4bc9cc558d0516c476eff490
Colin Xiong <xiong.colin899@gmail.com>
2023-01-21 12:25:55 -0500

    Install library and data.txt

--- frontend/passwordsite/src/components/zxcvbn.js
+++ frontend/passwordsite/src/components/zxcvbn.js
@@ -1,9 +1,14 @@
 import React from 'react'
+var zxcvbn = require('zxcvbn')
 
 const Zxcvbn = () => {
   return (
     <div>Zxcvbn</div>
   )
 }
+
+var passwordData = zxcvbn('Tr0ub4dour&3')
+
+console.log(passwordData);
 
 export default Zxcvbn
