3e8cdb21ba153c5fb55cdc2435e4bb3d6812686e
Thanh-Huy1104 <etienne01.nguyen@gmail.com>
2023-01-21 15:56:03 -0500

    825

--- frontend/passwordsite/src/components/PasswordChecker.js
+++ frontend/passwordsite/src/components/PasswordChecker.js
@@ -17,7 +17,7 @@ const PasswordChecker = () => {
   const toInt = (value) => {
     return parseInt(JSON.stringify(value));
   }
-  
+
   return (
     <div className='input__field__wrapper'>
         <div className='input__field__container'>
@@ -28,7 +28,7 @@ const PasswordChecker = () => {
             <pre className='input__text'>
               Password Score: {JSON.stringify(result['PasswordScore']['score'], null, 1)}
             </pre>
-            <LinearProgress variant='determinate' value={toInt(result['PasswordScore']['score'])*20}/>
+            <LinearProgress variant='determinate' value={toInt(result['PasswordScore']['score'])*25}/>
         </div>
      
     </div>
