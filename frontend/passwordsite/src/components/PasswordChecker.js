f8df17decc4c59fc5755a4b6000969f4b54a5101
Colin Xiong <xiong.colin899@gmail.com>
2023-01-21 14:28:19 -0500

    Implement password checker, data visuals (beta)

--- frontend/passwordsite/src/components/PasswordChecker.js
+++ frontend/passwordsite/src/components/PasswordChecker.js
@@ -3,39 +3,30 @@ import {Container} from 'react-bootstrap'
 var checkFor = require('zxcvbn');
 
 const PasswordChecker = () => {
-  const [password, setPassword] = useState('');
-  
-  const checkPassword = () => {
-    const result = checkFor(password);
-    console.log(result);
+  const [result, setResult] = useState({});
+
+  const setPassword = (event) => {
+    let PasswordScore = checkFor(event.target.value);
+    setResult(result => ({
+      PasswordScore
+    }));
   }
 
   return (
     <div>
       <Container>
         <div>
-            <input onChange={(event) => setPassword(event.target.value)} type="text" id="password" className="input__field" placeholder='Password'>
+            <input onChange={(event) => setPassword(event)} type="text" id="password" defaultValue={result.item1} className="input__field" placeholder='Password'>
             </input>
         </div>
-        <div className='Buttons'>
-            <button onClick={checkPassword} id="btn1">Check my password</button>
+        <div>
+            <pre>
+              Password Score: {JSON.stringify(result['PasswordScore'], null, 1)}
+            </pre>
         </div>
       </Container>
     </div>
   )
 }
 
-var passwordData = checkFor('ok')
-
-const password = passwordData['password']
-const score = passwordData['score']
-const feedback = passwordData['feedback']
-const numGuesses = passwordData['guesses']
-const feedbackSuggestions = passwordData['feedback']['suggestions']
-
-const data = JSON.stringify(passwordData)
-
-console.log(data);
-console.log(passwordData['password']);
-
-export default PasswordChecker
+export default  PasswordChecker;
